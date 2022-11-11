74794aa837cd324a4aae46075b745f0ed63cddda
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2022-11-11 22:44:23 +0530

    Add files via upload

---
+++ eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "root": true,
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": { "project": ["./tsconfig.json"] },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+      "@typescript-eslint/strict-boolean-expressions": [
+          2,
+          {
+              "allowString" : false,
+              "allowNumber" : false
+          }
+      ],
+      "comma-style": ["error", "first"]
+      , "comma-spacing": ["error", { "before": false, "after": true }]
+  },
+  "ignorePatterns": ["src/**/*.test.ts", "src/frontend/generated/*"]
+}
